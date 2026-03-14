033146693696bc2f25e053ff1299d3ea4a435336
OmPatel212 <92329571+OmPatel212@users.noreply.github.com>
2026-03-14 11:49:34 +0530

    setloginroutes

--- server/app.py
+++ server/app.py
@@ -1,12 +1,34 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from models import db, User
 
 app = Flask(__name__)
 CORS(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
 @app.route('/api/hello')
 def hello():
     return jsonify({"message": "Hello from Flask!"})
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    user = User.query.filter_by(email=email, password=password).first()
+
+    if not user:
+        return jsonify({"message": "Invalid email or password"}), 401
+
+    return jsonify({"message": "Login successful", "role": user.role, "name": user.name})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
---
+++ server/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique = True)
+    password = db.Column(db.String(100))
+    role = db.Column(db.String(20))   #role can be staff or manager (staff/manager)
+    
